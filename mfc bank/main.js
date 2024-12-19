e471d62713922e21ff61c53bf4d44b25554fac17
AbdulConsole <hzwedt@gmail.com>
2024-12-19 07:15:05 +0100

    update 🗃️

--- mfc bank/main.js
+++ mfc bank/main.js
@@ -1,8 +1,10 @@
+var time = 1000;
+
 window.addEventListener('load', () => {
   setTimeout(() => {
     const splash = document.querySelector('.splash-screen');
       if (splash) {
         splash.style.display = 'none';
       }
-  }, 2000);
+  }, time);
 });
